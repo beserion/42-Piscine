952f80caf90febdd3b287eb55bbc5d01c6af56ae
Yahya Karacan <hyk41hyk@gmail.com>
2025-12-01 21:34:50 +0300

    C00

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkaracan <hkaracan@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 21:33:49 by hkaracan          #+#    #+#             */
+/*   Updated: 2025/12/01 21:33:50 by hkaracan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long	nu;
+
+	nu = nb;
+	if (nu < 0)
+	{
+		ft_putchar('-');
+		nu = -nu;
+	}
+	if (nu / 10 != 0)
+		ft_putnbr(nu / 10);
+	ft_putchar(nu % 10 + 48);
+}
+
+int	main()
+{
+	ft_putnbr(5849);
+
+	return (0);
+}
